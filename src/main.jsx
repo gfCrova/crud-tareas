9d14889f872d72641345fac9a871a2f4b68e7b0c
gfCrova <giancrova.cl5@gmail>
2023-06-13 00:46:41 -0300

    add provider and children App

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
+import {TareasContextProvider} from './context/TareasContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <TareasContextProvider>
+      <App/>
+    </TareasContextProvider>
   </React.StrictMode>,
 )
