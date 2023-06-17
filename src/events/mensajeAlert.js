a5b67e6d9dc49e5fac953d4d87ebacb162ed07cf
gfCrova <giancrova.cl5@gmail.com>
2023-06-17 02:12:13 -0300

    add function event mensaje de alerta

--- src/events/mensajeAlert.js
+++ src/events/mensajeAlert.js
@@ -1,14 +1,19 @@
 
-export const mostrarMensaje = (param1, param2, colorCss) => {
+export const mostrarMensaje = (
+    mensaje, 
+    setInContainer, 
+    setInForm, 
+    colorCss
+    ) => {
 
     const div = document.createElement('div');
     div.className = `alertDiv absolute rounded-md p-3 ${colorCss} mt-2`;
-    div.appendChild(document.createTextNode("Se agregó satisfactoriamente"));
-    // Mostrando el el DOM
-    const container = document.querySelector(`${param1}`);
-    const form = document.querySelector(`${param2}`);
+    div.appendChild(document.createTextNode(mensaje));
+
+    const container = document.querySelector(`${setInContainer}`);
+    const form = document.querySelector(`${setInForm}`);
     container.insertBefore(div, form);
-    // Eliminando el mensaje automaticamente
+
     setTimeout(() => {
         document.querySelector('.alertDiv').remove();
     }, 2500);
